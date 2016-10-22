51ec3ee56f7c4de7157e1a28e38a530e0b89992f
SergioRivera1 <sergiorivera.jdr@gmail.com>
2016-10-22 13:52:03 -0600

    Agregando solucion y programa de consola

---
+++ Biblioteca/Blioteca.Consola/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blioteca.Consola 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            Console.ReadKey();
+            
+        }
+    }
+}
